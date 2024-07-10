71f08c8ab1410266225b8623a9114f4f601c7e22
minhein100 <172167655+minhein100@users.noreply.github.com>
2024-07-10 14:15:08 +0900

    Add files via upload

---
+++ js/1030yomi4.js
@@ -0,0 +1,80 @@
+$(document).ready(function () {
+
+	var colorArray = ["#019875", "#1E8BC3", "#74B72E"];
+	var cardState;
+	var currentQuestion = 0;
+	var qbank = [
+		["傾向", "けいこう"],
+		["施設", "しせつ"],
+		["候補", "こうほ"],
+		["絞る", "しぼる"],
+		["主人公", "しゅじんこう"],
+		["職人", "しょくにん"],
+		["車椅子", "くるまいす"],
+		["骨折", "こっせつ"],
+		["校舎", "こうしゃ"],
+		["大幅に", "おおはばに"],
+		["初対面", "しょたいめん"],
+		["解放", "かいほう"],
+		["日帰り", "ひがえり"],
+		["空席", "くうせき"],
+		["凝る", "こる"],
+		["都心", "としん"],
+		["空き地", "あきち"],
+		["手入れ", "ていれ"],
+		["対策", "たいさく"],
+		["放す", "はなす"],
+		["放る", "ほうる"],
+		["継続", "けいぞく"],
+		["正常", "せいじょう"],
+		["異常", "いじょう"],
+		["固定", "こてい"]];
+
+
+	beginActivity();
+
+
+	function beginActivity() {
+		cardState = 0;
+		var color1 = colorArray[Math.floor(Math.random() * colorArray.length)];
+		$("#cardArea").empty();
+		$("#cardArea").append('<div id="card1" class="card">' + qbank[currentQuestion][0] + '</div>');
+		$("#cardArea").append('<div id="card2" class="card">' + qbank[currentQuestion][1] + '</div>');
+		$("#card1").css("background-color", color1);
+		$("#card2").css("background-color", "#34495E");
+		$("#card2").css("top", "200px");
+		$("#cardArea").on("click", function () {
+			if (cardState != 1) {
+				cardState = 1;
+				//togglePosition();
+				$("#card1").animate({ top: "-=200" }, 150, function () { cardState = 0; togglePosition(); });
+				$("#card2").animate({ top: "-=200" }, 150, function () { togglePosition2(); });
+			}//if
+		});//click function
+		currentQuestion++;
+		$("#buttonArea").empty();
+		$("#buttonArea").append('<div id="nextButton">次へ</div>');
+		$("#buttonArea").append('<div id="otherButton"> <a href=\"./1030class_yomikata_lesson.html\" style="text-decoration:none;">他の練習を選ぶ</a></div>');
+		$("#nextButton").on("click", function () {
+			if (currentQuestion < qbank.length) { beginActivity(); }
+			else { displayFinalMessage(); }
+		});//click function
+	}//beginactivity
+
+	function togglePosition() {
+		if ($("#card1").position().top == -200) { $("#card1").css("top", "200px"); };
+	}//toggle
+
+	function togglePosition2() {
+		if ($("#card2").position().top == -200) { $("#card2").css("top", "200px"); };
+	}//toggle2
+
+	function displayFinalMessage() {
+		$("#buttonArea").empty();
+		$("#cardArea").empty();
+		$("#cardArea").append('<div id="finalMessage"><p>終了！<br>よく頑張ったね！</p></div>');
+		$("#buttonArea").append('<div id="otherButton"> <a href=\"./1030yomi3.html\" style="text-decoration:none;">最初からやり直す</a></div>');
+		$("#buttonArea").append('<div id="otherButton"> <a href=\"./1030class_yomikata_lesson.html\" style="text-decoration:none;">他の練習を選ぶ</a></div>');
+	}//final message
+
+});
