1bf60b7623246c13c6b61585fd850142bddc3005
minhein100 <172167655+minhein100@users.noreply.github.com>
2024-07-13 20:50:54 +0900

    Update 1030multi1.js

--- js/1030multi1.js
+++ js/1030multi1.js
@@ -49,66 +49,115 @@ const quizData = [
     options: ["あげる　", "なげる　", "かかげる", "こげる　"],
     answer: "かかげる"
   },
-  ];
+];
   
-  const questionElement = document.getElementById("question");
-  const optionsElement = document.getElementById("options");
-  // const submitButton = document.getElementById("submit");
-  
-  let currentQuestion = 0;
-  let score = 0;
-  
-  function showQuestion() {
-    const question = quizData[currentQuestion];
-    questionElement.innerText = question.question;
-  
-    optionsElement.innerHTML = "";
-    question.options.forEach(option => {
-      const button = document.createElement("button");
-      button.innerText = option;
-      optionsElement.appendChild(button);
-      button.addEventListener("click", selectAnswer);
-    });
+const questionElement = document.getElementById("question");
+const optionsElement = document.getElementById("options");
+let currentQuestion = 0;
+let score = 0;
+
+function beginTestWith() {
+  if(document.getElementById("withAnswer").checked == true) {
+    document.getElementById("withAnswer").disabled = true;
+    document.getElementById("withoutAnswer").disabled = true;
+    flowMsg();
+    showQuestionResult();
   }
-  
-  function selectAnswer(e) {
-    const selectedButton = e.target;
-    const answer = quizData[currentQuestion].answer;
-  
-    if (selectedButton.innerText === answer) {
-      score++;
-    }
-  
-    currentQuestion++;
-  
-    if (currentQuestion < quizData.length) {
-      showQuestion();
-    } else {
-      showResult();
-    }
+}
+
+function beginTestWithout() {
+  if(document.getElementById("withoutAnswer").checked == true) {
+    document.getElementById("withAnswer").disabled = true;
+    document.getElementById("withoutAnswer").disabled = true;
+    flowMsg();
+    showQuestionOnly();
   }
-  
-  function showResult() {
-    if (score < (quizData.length / 2)) {
-      multiChoiceArea.innerHTML = `
-      <h1>　Test Completed!</h1>
-      <p style="font-size:30px; color:red;">　スコア: ${score}/${quizData.length}</p>
-      <p style="font-size:20px; color:red; font-weight:bold;">　　残念！もう一度勉強しましょう！</p>
-    `;
-    } else if(score >= ((quizData.length / 10) * 8)) {
-      multiChoiceArea.innerHTML = `
-      <h1>　Test Completed!</h1>
-      <p style="font-size:30px; color:green;">　スコア: ${score}/${quizData.length}</p>
-      <p style="font-size:20px; color:green; font-weight:bold;">　　おめでとう！よくできました。</p>
-    `;
-    } else {
-      multiChoiceArea.innerHTML = `
-      <h1>　Test Completed!</h1>
-      <p style="font-size:30px;">　スコア: ${score}/${quizData.length}</p>
-      <p style="font-size:20px; font-weight:bold;">　　満点を目指しましょう！</p>
-    `;
-    }
-    
+}
+
+function showQuestionResult() {
+  const question = quizData[currentQuestion];
+  questionElement.innerText = question.question;
+  optionsElement.innerHTML = "";
+
+  question.options.forEach(option => {
+    const button = document.createElement("button");
+    button.innerText = option;
+    optionsElement.appendChild(button);
+    button.addEventListener("click", selectWithAnswer);
+  });
+}
+
+function showQuestionOnly() {
+  const question = quizData[currentQuestion];
+  questionElement.innerText = question.question;
+  optionsElement.innerHTML = "";
+
+  question.options.forEach(option => {
+    const button = document.createElement("button");
+    button.innerText = option;
+    optionsElement.appendChild(button);
+    button.addEventListener("click", selectWithoutAnswer);
+  });
+}
+
+var msg = "　　　4つの選択肢の中から一番適切なものを選んでください。　　　";
+var scr_speed = 400;	//スクロール速度
+function flowMsg() {
+  document.forms[0].flowMsg.value = msg
+  msg = msg.substring(1,msg.length) + msg.substring(0,1);
+  flowSpeed = setTimeout("flowMsg()", scr_speed);
+}
+
+function selectWithAnswer(e) {
+  const selectedButton = e.target;
+  const answer = quizData[currentQuestion].answer;
+  if (selectedButton.innerText === answer) {
+    score++;
+    finishedQuestion = currentQuestion + 1;
+    document.getElementById("showResult").value = finishedQuestion + "番。" + "正解です！" + "   Score:" + score + "/" + finishedQuestion;
+  }else {
+    finishedQuestion = currentQuestion + 1;
+    document.getElementById("showResult").value = finishedQuestion + "番。" + "不正解です！" + "   Score:" + score + "/" + finishedQuestion;
   }
-  
-  showQuestion();
+
+  currentQuestion++;
+
+  if (currentQuestion < quizData.length) {
+    showQuestionResult();
+  } else {
+    showResult();
+  }
+}
+
+function selectWithoutAnswer(e) {
+  const selectedButton = e.target;
+  const answer = quizData[currentQuestion].answer;
+  if (selectedButton.innerText === answer) {
+    score++;
+  }
+  currentQuestion++;
+  if (currentQuestion < quizData.length) {
+    showQuestionOnly();
+  } else {
+    showResult();
+  }
+}
+
+function showResult() {
+  if (score < (quizData.length / 2)) {
+    multiChoiceArea.innerHTML = `
+    <h1>　Test Completed!</h1>
+    <p style="font-size:30px; color:red;">　スコア: ${score}/${quizData.length}</p>
+    <p style="font-size:20px; color:red; font-weight:bold;">　　残念！もう一度勉強しましょう！</p>`;
+  } else if(score >= ((quizData.length / 10) * 8)) {
+    multiChoiceArea.innerHTML = `
+    <h1>　Test Completed!</h1>
+    <p style="font-size:30px; color:green;">　スコア: ${score}/${quizData.length}</p>
+    <p style="font-size:20px; color:green; font-weight:bold;">　　おめでとう！よくできました。</p>`;
+  } else {
+    multiChoiceArea.innerHTML = `
+    <h1>　Test Completed!</h1>
+    <p style="font-size:30px;">　スコア: ${score}/${quizData.length}</p>
+    <p style="font-size:20px; font-weight:bold;">　　満点を目指しましょう！</p>`;
+  }
+}
