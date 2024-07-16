82fdeeae02dcd0048c71929109dada665ecf7342
minhein100 <172167655+minhein100@users.noreply.github.com>
2024-07-16 11:07:35 +0900

    Update 0830yomi4.js

--- js/0830yomi4.js
+++ js/0830yomi4.js
@@ -73,7 +73,7 @@ $(document).ready(function () {
 		$("#buttonArea").empty();
 		$("#cardArea").empty();
 		$("#cardArea").append('<div id="finalMessage"><p>終了！<br>よく頑張ったね！</p></div>');
-		$("#buttonArea").append('<div id="otherButton"> <a href=\"./0830yomi3.html\" style="text-decoration:none;">最初からやり直す</a></div>');
+		$("#buttonArea").append('<div id="otherButton"> <a href=\"./0830yomi4.html\" style="text-decoration:none;">最初からやり直す</a></div>');
 		$("#buttonArea").append('<div id="otherButton"> <a href=\"./830class_yomikata_lesson.html\" style="text-decoration:none;">他の練習を選ぶ</a></div>');
 	}//final message
 
