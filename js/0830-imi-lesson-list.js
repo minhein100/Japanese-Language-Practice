0103bbfdc6d27e07e6ae42b8e3e264a02569925d
minhein100 <172167655+minhein100@users.noreply.github.com>
2024-08-02 22:32:38 +0900

    Update 0830-imi-lesson-list.js

--- js/0830-imi-lesson-list.js
+++ js/0830-imi-lesson-list.js
@@ -7,8 +7,8 @@ $(document).ready(function () {
     // .append('<div id="lessonListButton5"><a href=\"./0830multi3.html\" style="text-decoration:none;">０３</a></div>');
     $("#lessonListArea1")
     .append('<div id="lessonListButton5"><a href=\"./0830imi4.html\" style="text-decoration:none;">０４</a></div>');
-    // $("#lessonListArea1")
-    // .append('<div id="lessonListButton"><a href=\"830class.html\" style="text-decoration:none;">０５</a></div>');
+    $("#lessonListArea1")
+    .append('<div id="lessonListButton5"><a href=\"./0830imi5.html\" style="text-decoration:none;">０５</a></div>');
     // $("#lessonListArea1")
     // .append('<div id="lessonListButton"><a href=\"830class.html\" style="text-decoration:none;">０６</a></div>');
     // $("#lessonListArea1")
